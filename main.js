6b3e8da203bce4476086601623b8caac244e05e0
Veronika Háková <veronika.hakova@gmail.com>
2022-02-25 12:38:05 +0100

    typo fix

--- main.js
+++ main.js
@@ -5,31 +5,31 @@ function hodKostkou() {
 
     if (vysledekHodu < 6) {
         document.getElementById('zprava').innerHTML = 'Padla ti ' + vysledekHodu + '. Zkus to znovu!';
-        
+
         if (vysledekHodu == 1) {
-            document.getElementById('kostka').src = '/obrazky/1.png';
+            document.getElementById('kostka').src = 'obrazky/1.png';
         }
 
         else if (vysledekHodu == 2) {
-            document.getElementById('kostka').src = '/obrazky/2.png';
+            document.getElementById('kostka').src = 'obrazky/2.png';
         }
 
         else if (vysledekHodu == 3) {
-            document.getElementById('kostka').src = '/obrazky/3.png';
+            document.getElementById('kostka').src = 'obrazky/3.png';
         }
 
         else if (vysledekHodu == 4) {
-            document.getElementById('kostka').src = '/obrazky/4.png';
+            document.getElementById('kostka').src = 'obrazky/4.png';
         }
 
         else if (vysledekHodu == 5) {
-            document.getElementById('kostka').src = '/obrazky/5.png';
+            document.getElementById('kostka').src = 'obrazky/5.png';
         }
     }
 
     else if (vysledekHodu === 6) {
         document.getElementById('zprava').innerHTML = 'Padla ti ' + vysledekHodu + '. Gratulujeme k výhře!';
-        document.getElementById('kostka').src = '/obrazky/6.png';
+        document.getElementById('kostka').src = 'obrazky/6.png';
     }
 
     else {
