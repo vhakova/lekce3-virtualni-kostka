8d731fc1e0c722c872c1428d53afec0b5c78a9b2
Veronika Háková <veronika.hakova@gmail.com>
2022-02-25 12:33:42 +0100

    hotová kostička

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 // tady je místo pro náš program
+
+function hodKostkou() {
+    let vysledekHodu = Math.ceil(Math.random() * 6);
+
+    if (vysledekHodu < 6) {
+        document.getElementById('zprava').innerHTML = 'Padla ti ' + vysledekHodu + '. Zkus to znovu!';
+        
+        if (vysledekHodu == 1) {
+            document.getElementById('kostka').src = '/obrazky/1.png';
+        }
+
+        else if (vysledekHodu == 2) {
+            document.getElementById('kostka').src = '/obrazky/2.png';
+        }
+
+        else if (vysledekHodu == 3) {
+            document.getElementById('kostka').src = '/obrazky/3.png';
+        }
+
+        else if (vysledekHodu == 4) {
+            document.getElementById('kostka').src = '/obrazky/4.png';
+        }
+
+        else if (vysledekHodu == 5) {
+            document.getElementById('kostka').src = '/obrazky/5.png';
+        }
+    }
+
+    else if (vysledekHodu === 6) {
+        document.getElementById('zprava').innerHTML = 'Padla ti ' + vysledekHodu + '. Gratulujeme k výhře!';
+        document.getElementById('kostka').src = '/obrazky/6.png';
+    }
+
+    else {
+        document.getElementById('zprava').innerHTML = 'Stala se nějaká chyba.';
+    }
+}
